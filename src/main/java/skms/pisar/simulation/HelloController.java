a4f0ac1591b7d3feedfc7b9c478be89dd9d670b9
thekrasich <91842995+thekrasich@users.noreply.github.com>
2025-03-23 15:27:55 +0200

    init

---
+++ src/main/java/skms/pisar/simulation/HelloController.java
@@ -0,0 +1,14 @@
+package skms.pisar.simulation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
